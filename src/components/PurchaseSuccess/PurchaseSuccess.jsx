c605b181f41c9321f9175c578ab6a3c2642f24b8
Avanish Gupta <91612116+Avanish-Gupta-CSE@users.noreply.github.com>
2024-08-19 11:28:08 +0530

    Update PurchaseSuccess.jsx

--- src/components/PurchaseSuccess/PurchaseSuccess.jsx
+++ src/components/PurchaseSuccess/PurchaseSuccess.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
-// import './PurchaseSuccess.css';
+import './PurchaseSuccess.css';
 
 const PurchaseSuccess = () => {
   const { clearCart } = useContext(CartContext);
